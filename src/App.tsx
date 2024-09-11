4b7c26bc4a970e53ee8149f84e783cf4c1be8d0a
verric <beijing-planes@hotmail.com>
2024-09-12 01:01:09 +1000

    WIP Day 1 - building and placing layout and main data table

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,87 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import data from "./assets/2024/monza/results.json"
+import Monza from "./assets/2024/monza/Italy_Circuit.avif"
+import clsx from "clsx"
+import { ArrowDownIcon, ArrowUpIcon, MinusIcon } from "@heroicons/react/24/solid"
+import { useState } from "react"
+
+function PositionChange({ starting, final }: { starting: string | number; final: string | number }) {
+  const change: number = (starting as number) - (final as number)
+  if (change < 0) {
+    return (
+      <>
+        <ArrowDownIcon className="size-6 text-red-500" />
+        <span>({change})</span>
+      </>
+    )
+  } else if (change === 0) {
+    return <MinusIcon className="size-6" />
+  } else if (change > 0) {
+    return (
+      <>
+        <ArrowUpIcon className="size-6 text-green-500" />
+        <span>(+{change})</span>
+      </>
+    )
+  } else {
+    return null
+  }
+}
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const [horizontal, setHorizontal] = useState(true)
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <main className="p-4 flex" style={{ flexDirection: horizontal ? "row" : "column" }}>
+      <section className="flex-grow">
+        <h2 onClick={() => setHorizontal(!horizontal)} className="text-4xl">
+          Race: Monza
+        </h2>
+        <div className="overflow-x-auto overflow-y-auto">
+          <table className="table table-lg">
+            <thead>
+              <tr>
+                <th>Pos</th>
+                <th>Driver</th>
+                <th>Team</th>
+                <th>Time/Retired</th>
+                <th>Fast Lap</th>
+              </tr>
+            </thead>
+            <tbody>
+              {data.map((datum, index) => (
+                <tr key={index} className={clsx("hover", `hover:bg-${datum.car.toLocaleLowerCase()}`)}>
+                  <td className="border-l-4 flex" style={{ borderLeftColor: datum.colour }}>
+                    {datum.pos}
+                    <PositionChange starting={datum.starting} final={datum.pos} />
+                  </td>
+                  <td>{datum.driver}</td>
+                  <td>{datum.car}</td>
+                  <td>{datum.time}</td>
+                  <td>{datum.fastestLap}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      </section>
+      <section className="p-4">
+        <h2 className="text-4xl">Circuit Info: Autodromo Nazionale Monza 🇮🇹</h2>
+        <div className="stats w-full">
+          <article className="stat">
+            <p className="stat-title">Laps</p>
+            <p className="stat-value">53</p>
+          </article>
+          <article className="stat">
+            <p className="stat-title">Lap Distance</p>
+            <p className="stat-value">5.793 km</p>
+          </article>
+          <article className="stat">
+            <p className="stat-title">Race Distance</p>
+            <p className="stat-value">306.72 km</p>
+          </article>
+        </div>
+        <img src={Monza} alt="monza circuit" />
+      </section>
+    </main>
   )
 }
 
---
+++ tailwind.config.js
@@ -0,0 +1,45 @@
+/** @type {import('tailwindcss').Config} */
+import daisyui from "daisyui"
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        "red-bull": {
+          DEFAULT: "rgb(54, 113, 198)",
+        },
+        mclaren: {
+          DEFAULT: "rgb(255, 128, 0)",
+        },
+        ferrari: {
+          DEFAULT: "rgb(232, 0, 32)",
+        },
+        mercedes: {
+          DEFAULT: "rgb(39, 244, 210)",
+        },
+        "aston-martin": {
+          DEFAULT: "rgb(34, 153, 113)",
+        },
+        rb: {
+          DEFAULT: "rgb(102, 146, 255)",
+        },
+        haas: {
+          DEFAULT: "rgb(182, 186, 189)",
+        },
+        alpine: {
+          DEFAULT: "rgb(0, 147, 204)",
+        },
+        williams: {
+          DEFAULT: "rgb(100, 196, 255)",
+        },
+        sauber: {
+          DEFAULT: "rgb(0, 0, 0)",
+        },
+      },
+    },
+    fontFamily: {
+      sans: ["Montserrat", "sans-serif"],
+    },
+  },
+  plugins: [daisyui],
+}
