8c95c4cc61b2dc6768d573b33fe87d3d47c5b6ac
verric <beijing-planes@hotmail.com>
2024-09-12 15:07:15 +1000

    added monza tyre guide and driver avg speed

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import data from "./assets/2024/monza/results.json"
 import Monza from "./assets/2024/monza/Italy_Circuit.avif"
+import MonzaTyreGuide from "./assets/2024/monza/monza_tyre_guide.avif"
 import clsx from "clsx"
 import { ArrowDownIcon, ArrowUpIcon, MinusIcon } from "@heroicons/react/24/solid"
 import { useState } from "react"
@@ -30,7 +31,7 @@ function PositionChange({ starting, final }: { starting: string | number; final:
 function App() {
   const [horizontal, setHorizontal] = useState(true)
   return (
-    <main className="p-4 flex" style={{ flexDirection: horizontal ? "row" : "column" }}>
+    <main className="max-h-screen p-4 flex" style={{ flexDirection: horizontal ? "row" : "column" }}>
       <section className="flex-grow">
         <h2 onClick={() => setHorizontal(!horizontal)} className="text-4xl">
           Race: Monza
@@ -43,7 +44,8 @@ function App() {
                 <th>Driver</th>
                 <th>Team</th>
                 <th>Time/Retired</th>
-                <th>Fast Lap</th>
+                <th>Fastest Lap</th>
+                <th>Avg Speed kph / mph</th>
               </tr>
             </thead>
             <tbody>
@@ -57,29 +59,35 @@ function App() {
                   <td>{datum.car}</td>
                   <td>{datum.time}</td>
                   <td>{datum.fastestLap}</td>
+                  <td>{datum.avgSpeed}</td>
                 </tr>
               ))}
             </tbody>
           </table>
         </div>
       </section>
-      <section className="p-4">
+      <section className="h-full p-4 flex-grow flex flex-col">
         <h2 className="text-4xl">Circuit Info: Autodromo Nazionale Monza 🇮🇹</h2>
-        <div className="stats w-full">
+
+        <div className="stats">
           <article className="stat">
             <p className="stat-title">Laps</p>
             <p className="stat-value">53</p>
           </article>
           <article className="stat">
             <p className="stat-title">Lap Distance</p>
-            <p className="stat-value">5.793 km</p>
+            <p className="stat-value">5.793 km / 3.600 mi</p>
           </article>
           <article className="stat">
             <p className="stat-title">Race Distance</p>
-            <p className="stat-value">306.72 km</p>
+            <p className="stat-value">306.72 km / 190.587 mi</p>
           </article>
         </div>
-        <img src={Monza} alt="monza circuit" />
+
+        <div>
+          <img src={Monza} alt="monza circuit" />
+          <img src={MonzaTyreGuide} alt="monza tyre guide" />
+        </div>
       </section>
     </main>
   )
